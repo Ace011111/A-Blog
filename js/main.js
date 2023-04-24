c0ae400744bd385ec2ad8912af853461b395b6bf
Ace011111 <aqy-m-@hotmail.co.uk>
2023-04-24 19:12:31 +0100

    nightmode

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const menuBtn = document.querySelector('.menu-btn');
 const lines = document.querySelectorAll('.line');
 const navUl = document.querySelector('[nav-ul]');
-
+const themeBtn = document.querySelector('[theme-btn]');
+const themeBtnMoonImg = document.querySelector('#theme-btn-moon-img');
+const themeBtnSunImg = document.querySelector('#theme-btn-sun-img');
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('active');
     navUl.classList.toggle('active');
@@ -9,4 +11,10 @@ menuBtn.addEventListener('click', () => {
 
 navUl.addEventListener('click', () => {
     
-})
+});
+
+themeBtn.addEventListener('click', () => {
+    themeBtn.classList.toggle('active');
+
+    
+});
